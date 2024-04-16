61440132815d02321cd51f1e17c6f3c9f112e7b5
Samir-Lora <slora@wawand.co>
2024-04-16 15:28:25 -0500

    Initial Commit

---
+++ NovaCalendar/NovaCalendar.h
@@ -0,0 +1,18 @@
+//
+//  NovaCalendar.h
+//  NovaCalendar
+//
+//  Created by Samir on 16/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NovaCalendar.
+FOUNDATION_EXPORT double NovaCalendarVersionNumber;
+
+//! Project version string for NovaCalendar.
+FOUNDATION_EXPORT const unsigned char NovaCalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NovaCalendar/PublicHeader.h>
+
+
